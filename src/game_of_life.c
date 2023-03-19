96b4d5a1c0bb62e1d82ec3f11a279cfc8166a2d1
Combine Beverly <combineb@ev-d6.kzn.21-school.ru>
2023-03-19 12:16:49 +0300

    Score first

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ncurses.h>
 
-#define WIDTH 80 
+#define WIDTH 80
 #define HEIGHT 25
 
 void read_file(int a[HEIGHT][WIDTH], int *flag);
@@ -9,38 +10,48 @@ void view(int a[HEIGHT][WIDTH], int *flag);
 int neighbors(int a[HEIGHT][WIDTH], int x, int y);
 void make_new(int a[HEIGHT][WIDTH], int b[HEIGHT][WIDTH]);
 void swap(int a[HEIGHT][WIDTH], int b[HEIGHT][WIDTH]);
+int speed(int *flag);
 
 int main() {
     int current_array[HEIGHT][WIDTH] = {{current_array[0][0] = 0}};
     int new_array[HEIGHT][WIDTH] = {{new_array[0][0] = 0}};
     int flag = 0;
-    char tap;
-    
+
     read_file(current_array, &flag);
-    while(flag == 0) {
-        scanf("%c", &tap);
-        if (tap == '\n') {
-            view (current_array, &flag);
-            make_new(current_array, new_array);
-            swap(current_array, new_array);
-        } else {
-            flag = 1;
-        }
+    initscr();
+    int seed = speed(&flag);
+    halfdelay (seed);
+    
+    while (flag == 0) {
+        //scanf("%c", &tap);
+        //if (tap == '\n') {
+            view(current_array, &flag);
+            int tr = getch();
+	        switch(tr) {
+                case ERR: 
+                    make_new(current_array, new_array);
+                    swap(current_array, new_array);
+	                break;
+                default:
+                    flag = 1;
+            }
+        //} else {
+            //flag = 1;
     }
-    printf("Игра закончена!");
+    endwin();
     return 0;
 }
 
 void read_file(int a[HEIGHT][WIDTH], int *flag) {
     int figure = 1;
-    printf("Выбери начальную инициализацию(от 1 до 5): ");
+    printf("Выбери начальную инициализацию (от 1 до 5): ");
     int b = scanf("%d", &figure);
     FILE *file = NULL;
     if (b != 1) {
         printf("Вводи целое число от 1 до 5!:)\n");
         *flag = 1;
     } else {
-       if (figure == 1) {
+        if (figure == 1) {
             file = fopen("GLIDER.txt", "r");
         } else if (figure == 2) {
             file = fopen("HEART.txt", "r");
@@ -50,25 +61,25 @@ void read_file(int a[HEIGHT][WIDTH], int *flag) {
             file = fopen("LETTER.txt", "r");
         } else if (figure == 5) {
             file = fopen("CUSTOM.txt", "r");
-        } 
+        }
     }
-
-    char c;
-    int cnt = 0;
+ 
     if (file == NULL) {
         printf("Файл не найден\n");
         *flag = 1;
     } else {
-        while ((c = getc(file)) != EOF){
+        char c;
+        int cnt = 0;
+        while ((c = getc(file)) != EOF) {
             if (c == '0') {
                 a[cnt / WIDTH][cnt % WIDTH] = 0;
-                cnt +=1;
+                cnt += 1;
             }
             if (c == '1') {
                 a[cnt / WIDTH][cnt % WIDTH] = 1;
-                cnt +=1;
+                cnt += 1;
             }
-        }                
+        }
     }
     fclose(file);
 }
@@ -94,8 +105,8 @@ void view(int a[HEIGHT][WIDTH], int *flag) {
 
 int neighbors(int a[HEIGHT][WIDTH], int x, int y) {
     int count = 0;
-    int string = 0; 
-    int column = 0; 
+    int string = 0;
+    int column = 0;
     for (int i = y - 1; i <= y + 1; i++) {
         for (int j = x - 1; j <= x + 1; j++) {
             if (i < 0) {
@@ -113,7 +124,7 @@ int neighbors(int a[HEIGHT][WIDTH], int x, int y) {
             } else {
                 string = i;
                 column = j;
-            } 
+            }
             count += a[string][column];
         }
     }
@@ -122,7 +133,7 @@ int neighbors(int a[HEIGHT][WIDTH], int x, int y) {
 }
 
 void make_new(int a[HEIGHT][WIDTH], int b[HEIGHT][WIDTH]) {
-    int neighbor = 0; 
+    int neighbor = 0;
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             neighbor = neighbors(a, j, i);
@@ -147,5 +158,25 @@ void swap(int a[HEIGHT][WIDTH], int b[HEIGHT][WIDTH]) {
         for (int j = 0; j < WIDTH; j++) {
             a[i][j] = b[i][j];
         }
-    }    
+    }
+}
+
+int speed(int *flag) {
+    int res;
+    int score;
+    printw("Enter a speed from 1 to 5: ");
+    scanw("%d", &score);
+    if (score == 5) {
+	res = 1;
+    } else if (score == 4) {
+	res = 5;
+    } else if (score == 3) {
+	res = 10;
+    } else if (score == 2) {
+	res = 25;
+    } else if (score == 1) {
+	res = 50;
+    } else
+	*flag = 1;
+    return res;
 }
